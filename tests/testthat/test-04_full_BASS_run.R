2bce6547276a4c3e43c89c463c578febe92caac1
steffilazerte <steffi@steffi.ca>
2023-01-16 16:01:55 -0600

    Fix seed problems with full run

--- tests/testthat/test-04_full_BASS_run.R
+++ tests/testthat/test-04_full_BASS_run.R
@@ -4,13 +4,11 @@ test_that("full_BASS_run()", {
   n_samples <- 3
 
   expect_message({
-    withr::with_seed(1234, {
-      f1 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
-                          att = psu_land_cover, att.sp = psu_hexagons,
-                          HexID_ = hex_id,
-                          stratumID = province,
-                          noCost = TRUE, cost = NA)
-    })
+    f1 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
+                        att = psu_land_cover, att.sp = psu_hexagons,
+                        HexID_ = hex_id, seed_ = 1234,
+                        stratumID = province,
+                        noCost = TRUE, cost = NA)
   })
 
   expect_s3_class(f1, "data.frame")
@@ -18,13 +16,11 @@ test_that("full_BASS_run()", {
   expect_named(f1, c("hex_id", "benefit", "run", "num_runs", "nsamples"))
 
   expect_message({
-    withr::with_seed(1234, {
-      f2 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
-                          att = psu_land_cover, att.sp = psu_hexagons,
-                          HexID_ = hex_id,
-                          stratumID = province,
-                          noCost = FALSE, cost = psu_costs)
-    })
+    f2 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
+                        att = psu_land_cover, att.sp = psu_hexagons,
+                        HexID_ = hex_id, seed_ = 1234,
+                        stratumID = province,
+                        noCost = FALSE, cost = psu_costs)
   })
 
   expect_s3_class(f2, "data.frame")
@@ -33,13 +29,13 @@ test_that("full_BASS_run()", {
                     "weightedIP", "inclpr", "num_runs", "nsamples") %in%
                     names(f2)))  # No run?
 
-  # Not the same????? - BECAUSE   set.seed(seed_) on L37?
-  # expect_equal(dplyr::select(f1, -"run") %>% dplyr::arrange(hex_id, benefit),
-  #              dplyr::select(f2, dplyr::any_of(names(f1))) %>%
-  #                sf::st_drop_geometry() %>%
-  #                dplyr::arrange(hex_id, benefit))
+  # Expect same benefits
+  expect_equal(dplyr::select(f1, -"run") %>% dplyr::arrange(hex_id, benefit),
+               dplyr::select(f2, dplyr::any_of(names(f1))) %>%
+                 sf::st_drop_geometry() %>%
+                 dplyr::arrange(hex_id, benefit))
 
-  # Snapshots cannot be tested interactively - WHY NOT CONSISTENT?
-#  expect_snapshot_value(f1, style = "json2", tolerance = 0.0004)
- # expect_snapshot_value(f2, style = "json2", tolerance = 0.0004)
+  # Snapshots cannot be tested interactively
+  expect_snapshot_value(f1, style = "json2", tolerance = 0.0004)
+  expect_snapshot_value(f2, style = "json2", tolerance = 0.0004)
 })
