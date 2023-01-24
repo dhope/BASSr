db5e10d9bd4c4ee5b40f8333ece421780a68f190
steffilazerte <steffi@steffi.ca>
2023-01-24 16:27:09 -0600

    Update full run with cost/benefit function changes - Move specific checks to downstream functions

--- R/BASS_full_run.R
+++ R/BASS_full_run.R
@@ -13,20 +13,20 @@
 #' @param non_ran_set Non random set that is added to the hypothetical sample set in benefit calculation.
 #' @param lakeN The land cover number to represent open water. 1 for FNLC, 18 for CLC
 #' @param benefit_weight The weight assigned to benefit in the selection probabilities.0.5 is equal weighting of cost and benefits. 1.0 is zero weighting to cost.
-#' @param noCost Do you only want to run the benefit calculation?
 #' @param weighted_benefits data frame with lc and weights
 #'
 #' @return a table with inclusion probabilities
 #' @export
 #'
-full_BASS_run <- function(num_runs, nsamples, att, att.sp, cost,
-                          return_all = FALSE, seed_ = as.integer(Sys.time()),
+full_BASS_run <- function(num_runs, nsamples, att, att.sp, costs = NULL,
+                          seed_ = as.integer(Sys.time()),
                           HexID_ = HEX100, stratumID = StudyAreaID,
-                          quick = FALSE, non_ran_set = NULL, lakeN = 18,
-                          benefit_weight = 0.5, noCost = F,
-                          weighted_benefits = NULL, quiet = FALSE) {
+                          non_ran_set = NULL, lakeN = 18,
+                          benefit_weight = 0.5,
+                          weighted_benefits = NULL, return_grts = FALSE,
+                          quiet = FALSE) {
 
-  # Input Checks
+  # CHECK
 
   # Check if habitat and sp object have same numbers of rows
   if(nrow(att.sp)!= nrow(att)) rlang::abort(message = "att.sp and att must be the same size. They do not currently have the same number of rows")
@@ -40,12 +40,6 @@ full_BASS_run <- function(num_runs, nsamples, att, att.sp, cost,
 
   set.seed(seed_)
 
-  if (!"INLAKE" %in% names(cost) & !isTRUE(noCost)) {
-    if(!quiet) message("Did you forget to add a lake specification to the cost? I am addig it based on dominant land cover for now.")
-    inlake <- NA
-    try(inlake <- dplyr::filter_at(att, dplyr::vars(dplyr::matches("^D_.+")), dplyr::any_vars(. == lakeN)))
-    cost <- dplyr::mutate(cost, INLAKE = {{ HexID_ }} %in% inlake$ET_Index)
-  }
   if (!"sf" %in% class(att.sp)) {
     stop("Spatial object att.sp must be an object of package sf. Please fix and try again")
   } else {
@@ -54,86 +48,47 @@ full_BASS_run <- function(num_runs, nsamples, att, att.sp, cost,
       att.sp <- sf::st_centroid(att.sp)
     }
   }
-  if (!"X" %in% names(cost) & !isTRUE(noCost)) {
-    if(!quiet) message("Did you forget to add coordinates? I am adding it based centroids of the att.sp for now.")
-
-    cost <- sf::st_centroid(cost)
-    cost <- cost %>%
-      sf::st_coordinates() %>%
-      dplyr::as_tibble() %>%
-      dplyr::bind_cols(cost)
-  }
 
   if (nsamples ==0) grts_output <- NULL
   if (nsamples !=0) {
 
-  grts_output <- draw_random_samples(att_cleaned = att, att.sf = att.sp,
-                                     num_runs = num_runs, nsamples = nsamples,
-                                     quiet = quiet)
-  if(!quiet) message("sample draw complete")
+    grts_output <- draw_random_samples(att_cleaned = att, att.sf = att.sp,
+                                       num_runs = num_runs, nsamples = nsamples,
+                                       quiet = quiet)
+    if(!quiet) message("sample draw complete")
   }
 
   att_cleaned_long <- prepare_hab_long(att, {{ stratumID }})
 
-  if (!isTRUE(return_all)) {
-    benefits <- calculate_benefit(
-      grts_res = grts_output, att_long = att_cleaned_long,
-      non_random_set = non_ran_set, output = "benefit_by_run",
-      HexID = {{ HexID_ }}, quick = quick,
-      land_cover_weights = weighted_benefits
-    )
+  # Benefits
+  benefits <- calculate_benefit(
+    grts_res = grts_output, att_long = att_cleaned_long,
+    non_random_set = non_ran_set,
+    HexID = {{ HexID_ }},
+    land_cover_weights = weighted_benefits
+  )
 
-    if (isTRUE(noCost)) {
-      run_results <- dplyr::mutate(benefits,
-                                   num_runs = .env$num_runs,
-                                   nsamples = .env$nsamples)
-    } else {
-      pointswith_inclusion <- calculate_inclusion_probs(
-        cost = cost, hexagon_benefits = benefits, HexID = {{ HexID_ }},
-        StratumID = {{ stratumID }}, benefit_weight = benefit_weight)
-
-      run_results <- dplyr::mutate(pointswith_inclusion,
-                                   num_runs = .env$num_runs,
-                                   nsamples = .env$nsamples)
-    }
+  # Costs
+  if (!is.null(costs)) {
+    r <- calculate_inclusion_probs(
+      costs = costs, benefits = benefits, HexID = {{ HexID_ }},
+      StratumID = {{ stratumID }}, benefit_weight = benefit_weight)
+    type <- "inclusion_probs"
+  } else {
+    r <- benefits
+    type <- "benefits"
   }
 
-  if (isTRUE(return_all)) {
-    benefits <- calculate_benefit(
-      grts_res = grts_output, att_long = att_cleaned_long,
-      non_random_set = non_ran_set, output = "all",
-      HexID = {{ HexID_ }}, quick = quick,
-      land_cover_weights = weighted_benefits
-    )
+  # Meta
+  r <- dplyr::mutate(r,
+                     num_runs = .env$num_runs,
+                     nsamples = .env$nsamples)
 
 
-    if (isTRUE(quick)) b <- benefits else b <- benefits$hexagon_benefits
+  # GRTS
+  if(return_grts) r <- setNames(list(r, grts_output), c(type, "grts_output"))
 
-    if (isTRUE(noCost)) {
-      run_results <- dplyr::mutate(b,
-                                   num_runs = .env$num_runs,
-                                   nsamples = .env$nsamples)
-    } else {
-
-      pointswith_inclusion <- calculate_inclusion_probs(
-        cost = cost, hexagon_benefits = b,
-        HexID = {{ HexID_ }},
-        StratumID = {{ stratumID }},
-        benefit_weight = benefit_weight
-      )
-
-      run_results <- list(
-        inclusionPr =  dplyr::mutate(pointswith_inclusion,
-                                     num_runs = .env$num_runs,
-                                     nsamples = .env$nsamples),
-        benefits_full = benefits,
-        grts_samples = grts_output,
-        att_long = att_cleaned_long
-      )
-    }
-  }
-
-  run_results
+  r
 }
 
 
--- tests/testthat/test-04_full_BASS_run.R
+++ tests/testthat/test-04_full_BASS_run.R
@@ -8,13 +8,12 @@ test_that("full_BASS_run()", {
     f1 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
                         att = psu_land_cover, att.sp = psu_hexagons,
                         HexID_ = hex_id, seed_ = 1234,
-                        stratumID = province,
-                        noCost = TRUE, cost = NA, quiet = TRUE)
+                        stratumID = province, quiet = TRUE)
   })
 
   expect_s3_class(f1, "data.frame")
-  expect_equal(nrow(f1), n_runs * nrow(psu_hexagons))
-  expect_named(f1, c("hex_id", "benefit", "run", "num_runs", "nsamples"))
+  expect_equal(nrow(f1), nrow(psu_hexagons))
+  expect_named(f1, c("hex_id", "benefit", "num_runs", "nsamples"))
 
 
   # With costs
@@ -23,95 +22,54 @@ test_that("full_BASS_run()", {
                         att = psu_land_cover, att.sp = psu_hexagons,
                         HexID_ = hex_id, seed_ = 1234,
                         stratumID = province,
-                        noCost = FALSE, cost = psu_costs, quiet = TRUE)
+                        costs = psu_costs, quiet = TRUE)
   })
 
   expect_s3_class(f2, "data.frame")
-  expect_equal(nrow(f2), n_runs * nrow(psu_hexagons))
+  expect_equal(nrow(f2), nrow(psu_hexagons))
   expect_true(all(c("benefit", "LogCost", "ScLogCost", "scale_ben", "partIP",
                     "weightedIP", "inclpr", "num_runs", "nsamples") %in%
-                    names(f2)))  # No run?
+                    names(f2)))
 
-  # Expect same benefits
-  expect_equal(dplyr::select(f1, -"run") %>% dplyr::arrange(hex_id, benefit),
-               dplyr::select(f2, dplyr::any_of(names(f1))) %>%
-                 sf::st_drop_geometry() %>%
-                 dplyr::arrange(hex_id, benefit))
-
-  # Snapshots cannot be tested interactively
-  expect_snapshot_value(f1, style = "json2", tolerance = 0.0004)
-  expect_snapshot_value(f2, style = "json2", tolerance = 0.0004)
-})
-
-test_that("full_BASS_run() slow", {
-
+  # With GRTS - No costs
   expect_silent({
-    f1 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
+    f3 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
                         att = psu_land_cover, att.sp = psu_hexagons,
-                        HexID_ = hex_id, quick = FALSE, seed_ = 1234,
-                        stratumID = province,
-                        noCost = TRUE, cost = NA, quiet = TRUE)
+                        HexID_ = hex_id, return_grts = TRUE,
+                        stratumID = province, seed_ = 1234,
+                        quiet = TRUE)
   })
 
-  expect_s3_class(f1, "data.frame")
-  expect_equal(nrow(f1), n_runs * nrow(psu_hexagons))
-  expect_named(f1, c("hex_id", "benefit", "run", "num_runs", "nsamples"))
+  expect_type(f3, "list")
+  expect_named(f3, c("benefits", "grts_output"))
+  expect_equal(nrow(f3[["benefits"]]), nrow(psu_hexagons))
 
   # With costs
   expect_silent({
-    f2 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
+    f4 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
                         att = psu_land_cover, att.sp = psu_hexagons,
-                        HexID_ = hex_id, quick = FALSE, seed_ = 1234,
-                        stratumID = province,
-                        noCost = FALSE, cost = psu_costs, quiet = TRUE)
+                        HexID_ = hex_id, return_grts = TRUE,
+                        stratumID = province, seed_ = 1234,
+                        costs = psu_costs, quiet = TRUE)
   })
 
-  expect_s3_class(f1, "data.frame")
-  expect_equal(nrow(f1), n_runs * nrow(psu_hexagons))
-  expect_named(f1, c("hex_id", "benefit", "run", "num_runs", "nsamples"))
+  expect_type(f4, "list")
+  expect_named(f4, c("inclusion_probs", "grts_output"))
+  expect_equal(nrow(f4[["inclusion_probs"]]), nrow(psu_hexagons))
 
   # Expect same benefits
-  expect_equal(dplyr::select(f1, -"run") %>% dplyr::arrange(hex_id, benefit),
-               dplyr::select(f2, dplyr::any_of(names(f1))) %>%
-                 sf::st_drop_geometry() %>%
-                 dplyr::arrange(hex_id, benefit))
+  nms <- names(f1)
+  expect_equal(f1, sf::st_drop_geometry(f2[nms]))
+  expect_equal(f3[["benefits"]],
+               sf::st_drop_geometry(f4[["inclusion_probs"]][nms]))
+  expect_equal(f1,
+               sf::st_drop_geometry(f4[["inclusion_probs"]][nms]))
+
+  # Expect same costs
+  expect_equal(f2, f4[["inclusion_probs"]])
 
   # Snapshots cannot be tested interactively
   expect_snapshot_value(f1, style = "json2", tolerance = 0.0004)
   expect_snapshot_value(f2, style = "json2", tolerance = 0.0004)
-
-})
-
-test_that("full_BASS_run() all", {
-
-  # All
-  expect_silent({
-    f1 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
-                        att = psu_land_cover, att.sp = psu_hexagons,
-                        HexID_ = hex_id, return_all = TRUE,
-                        stratumID = province, seed_ = 1234,
-                        noCost = TRUE, cost = NA, quiet = TRUE)
-  })
-
-  expect_s3_class(f1, "data.frame")
-  expect_equal(nrow(f1), n_runs * nrow(psu_hexagons))
-  #expect_named(f2, c("hex_id", "benefit", "run", "num_runs", "nsamples"))
-
-  # With costs
-  expect_silent({
-    f2 <- full_BASS_run(num_runs = n_runs, nsamples = n_samples,
-                        att = psu_land_cover, att.sp = psu_hexagons,
-                        HexID_ = hex_id, return_all = TRUE,
-                        stratumID = province, seed_ = 1234,
-                        noCost = FALSE, cost = psu_costs, quiet = TRUE)
-  })
-
-  expect_type(f2, "list")
-  expect_equal(nrow(f2[["inclusionPr"]]), n_runs * nrow(psu_hexagons))
-
-  # Snapshots cannot be tested interactively
-  expect_snapshot_value(f1, style = "json2", tolerance = 0.0004)
-  expect_snapshot_value(f2, style = "json2", tolerance = 0.0004)
-
 })
 
