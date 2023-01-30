ec38d66d228a019dadc5d58c1fba82f7da985778
steffilazerte <steffi@steffi.ca>
2023-01-30 14:51:35 -0600

    Move add_coords to utils

--- data-raw/data_create_study_area.R
+++ data-raw/data_create_study_area.R
@@ -17,18 +17,6 @@ raster::crs(nr) <- 3161
 bbx <- sf::st_bbox(nr) |>
   sf::st_as_sfc()
 
-add_coords <- function(sf) {
-  # Set attributes as constant to avoid sf warnings
-  # (cf. https://github.com/r-spatial/sf/issues/406)
-  sf <- sf::st_set_agr(sf, "constant") %>%
-    sf::st_centroid()
-
-  sf %>%
-    sf::st_coordinates() %>%
-    dplyr::as_tibble() %>%
-    dplyr::bind_cols(sf, .)
-}
-
 stars_df <- nr %>%
   stars::st_as_stars() %>%
   sf::st_as_sf()
